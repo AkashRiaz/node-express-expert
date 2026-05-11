555863759150911db32bbd6bc57ffdc8d645d3cc
Md Akashuzzaman Riaz <akashriaz05@gmail.com>
2026-05-11 22:49:57 +0600

    setup node project without express

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -1,8 +1,9 @@
 import type { IncomingMessage, ServerResponse } from "http";
 import { readProduct } from "../service/product.service";
 import type { IProduct } from "../type/product.type";
+import { parseBody } from "../utility/parseBody";
 
-export const productController = (
+export const productController = async (
   req: IncomingMessage,
   res: ServerResponse,
 ) => {
@@ -11,7 +12,7 @@ export const productController = (
   const urlParts = url?.split("/");
   const id =
     urlParts && urlParts[1] === "products" ? Number(urlParts[2]) : null;
-  console.log(id);
+  // console.log(req, "Request");
 
   if (url === "/products" && method === "GET") {
     const products = readProduct();
@@ -32,12 +33,13 @@ export const productController = (
         data: product,
       }),
     );
-  } else if (method === "POST" && url === "/product") {
-    const body = ""
+  } else if (method === "POST" && url === "/products") {
+    const body = await parseBody(req);
+    console.log(body, "this is body")
     res.writeHead(200, { "content-type": "application/json" }).end(
       JSON.stringify({
         message: "This is product route updatedddd",
-        data: products,
+        // data: products,
       }),
     );
   }
